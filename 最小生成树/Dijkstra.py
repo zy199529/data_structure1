b351bd3fad0dc2454d0793b853567b312944e1da
zy19950209 <823356427@qq.com>
2019-06-11 16:30:26 +0800

    维特比

--- 最小生成树/Dijkstra.py
+++ 最小生成树/Dijkstra.py
@@ -3,21 +3,17 @@
 # @Author: Lenovo
 # @Date:   2019-06-08 21:37:22
 # @Last Modified by:   zy19950209
-# @Last Modified time: 2019-06-08 23:20:18
+# @Last Modified time: 2019-06-09 09:16:09
 import sys
 max = sys.maxsize  # 设置无穷大
-primgraph = graph = [
-    [max, 6, 3, 1, max, max, max, max, max],
-    [6, max, 2, max, 1, max, max, max, max],
-    [3, 2, max, 2, max, max, max, max, max],
-    [1, max, 2, max, 6, 10, max, max, max],
-    [max, 1, max, 6, max, 4, 3, 6, 2],
-    [max, max, max, 10, 4, max, 2, max, max],
-    [max, max, max, max, 3, 2, max, 4, max],
-    [max, max, max, max, 6, max, 4, max, 3],
-    [max, max, max, max, 2, max, max, 3, max],
-]
-chararray = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+primgraph = graph = [[max, 12, max, max, max, 16, 14],
+                     [12, max, 10, max, max, 7, max],
+                     [max, 10, max, 3, 5, 6, max],
+                     [max, max, 3, max, 4, max, max],
+                     [max, max, 5, 4, max, 2, 8],
+                     [16, 7, 6, max, 2, max, 9],
+                     [14, max, max, max, 8, 9, max]]
+chararray = ['0', '1', '2', '3', '4', '5', '6']
 n = len(chararray)
 charlist = []
 charlist.append(chararray[0])
--- 最小生成树/prim.py
+++ 最小生成树/prim.py
@@ -3,20 +3,18 @@
 # @Author: Lenovo
 # @Date:   2019-06-07 21:07:16
 # @Last Modified by:   zy19950209
-# @Last Modified time: 2019-06-07 23:04:21
+# @Last Modified time: 2019-06-09 09:43:55
 # 使用prim算法进行最小生成树的计算
 import sys
 MAX = sys.maxsize  # 设置无穷大
-primgraph = [[MAX, 10, MAX, MAX, MAX, 11, MAX, MAX, MAX],
-             [10, MAX, 18, MAX, MAX, MAX, 16, MAX, 12],
-             [MAX, 18, MAX, 22, MAX, MAX, MAX, MAX, 8],
-             [MAX, MAX, 22, MAX, 20, MAX, MAX, 16, 21],
-             [MAX, MAX, MAX, 20, MAX, 26, 7, 19, MAX],
-             [11, MAX, MAX, MAX, 26, MAX, 17, MAX, MAX],
-             [MAX, 16, MAX, MAX, 7, 17, MAX, 19, MAX],
-             [MAX, MAX, MAX, 16, 19, MAX, 19, MAX, MAX],
-             [MAX, 12, 8, 21, MAX, MAX, MAX, MAX, MAX]]
-chararray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+primgraph =[[MAX, 6, 1, 5, MAX, MAX],
+     [6, MAX, 5, MAX, 3, MAX],
+     [1, 5, MAX, 5, 6, 4],
+     [5, MAX, 5, MAX, MAX, 2],
+     [MAX, 3, 6, MAX, MAX, 6],
+     [MAX, MAX, 4, 2, 6, MAX]]
+
+chararray = ['A', 'B', 'C', 'D', 'E', 'F']
 charlist = []  # 存放最小生成树的结点
 charlist.append(chararray[0])  # 存放第一个初始结点
 n = len(chararray)  # 总长度
@@ -29,14 +27,14 @@ for _ in range(1, n):
     min = MAX
     minid = 0
     for j in range(1, n):  # 寻找最小权重的结点
-    	if lowcost[j]!=-1 and lowcost[j]<min:
-    		min = lowcost[j]
-    		minid = j
-    charlist.append(chararray[minid]) #最小生成树加入一个节点
-    sum =sum+min
+        if lowcost[j] != -1 and lowcost[j] < min:
+            min = lowcost[j]
+            minid = j
+    charlist.append(chararray[minid])  # 最小生成树加入一个节点
+    sum = sum+min
     lowcost[minid] = -1
-    for j in range(1,n):
-    	if lowcost[j]!=-1 and lowcost[j]>primgraph[minid][j]:
-    		lowcost[j]=primgraph[minid][j]
+    for j in range(1, n):
+        if lowcost[j] != -1 and lowcost[j] > primgraph[minid][j]:
+            lowcost[j] = primgraph[minid][j]
 print(sum)
 print(charlist)
